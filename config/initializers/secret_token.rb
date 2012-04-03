78fb6957e3f396794c3e7e0c5b7187678e04e3f5
Jeremy Foucault <foucault.jeremy@gmail.com>
2012-04-03 12:23:08 +0200

    initial commit with rails files and folders

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sinhar::Application.config.secret_token = 'b0666fda2d8d788f11606298d6a2d07be3c3d806b4d07bd42dcd7e90252c44ca1d10dcdbe79c2c72c2e2b3492dcee7fa8e46bc966089032794ace3428a1d9058'
